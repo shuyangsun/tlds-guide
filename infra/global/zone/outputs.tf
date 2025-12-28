aa2bf36cb5e11d730a93a9b5de3f619a417bc52f
Shuyang Sun <shuyangsun10@gmail.com>
2025-12-28 15:50:02 -0500

    Remove nameservers from outputs.

--- infra/global/zone/outputs.tf
+++ infra/global/zone/outputs.tf
@@ -1,10 +1,5 @@
 # Path: infra/global/zone/outputs.tf
 
-output "cloudflare_nameservers" {
-  description = "Cloudflare nameservers assigned to the zone"
-  value       = cloudflare_zone.primary_domain.name_servers
-}
-
 output "cloudflare_zone_status" {
   description = "Cloudflare zone status (should be 'active' when nameservers are verified)"
   value       = cloudflare_zone.primary_domain.status
--- infra/main.tf
+++
@@ -1,5 +0,0 @@
-# Path: infra/main.tf
-
-terraform {
-  required_version = "~> 1.14.3"
-}
