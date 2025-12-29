160449f7e879c17d2fa069afd2cc8c123168ed13
Shuyang Sun <shuyangsun10@gmail.com>
2025-12-29 15:40:17 -0500

    Parallel tooling and staging infra deployment, and fix Terraform duplicated module bug.

--- infra/tooling/cicd/github_workflows/main.tf
+++ infra/tooling/cicd/github_workflows/main.tf
@@ -1,11 +1,11 @@
 # Path: infra/tooling/cicd/github_workflows/main.tf
 
-module "gh_workflows" {
+module "gh_workflows_staging" {
   source      = "../../../modules/tooling/github_workflows"
   environment = "staging"
 }
 
-module "gh_workflows" {
+module "gh_workflows_prod" {
   source      = "../../../modules/tooling/github_workflows"
   environment = "prod"
 }
