3465b889c0e7a04ac30cf278e30b9c7dff662648
Shuyang Sun <shuyangsun10@gmail.com>
2025-12-29 14:01:11 -0500

    Name bug fix.

--- infra/modules/tooling/github_workflows/locals.tf
+++ infra/modules/tooling/github_workflows/locals.tf
@@ -6,7 +6,7 @@ locals {
   gh_oidc_thumbprint = "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   gh_repo_branch     = "shuyangsun/tlds-guide:ref:refs/heads/main"
 
-  iam_policy_doc_name    = "GitHubActions${var.environment == "prod" ? "Staging" : "Prod"}AssumeRole"
+  iam_policy_doc_name    = "GitHubActions${var.environment == "prod" ? "Prod" : "Staging"}AssumeRole"
   iam_policy_name        = "terraform-state-access-${var.environment}"
   iam_role_name          = "github-actions-terraform-${var.environment}"
   state_s3_bucket        = "shuyang-tfstate-${var.environment}-us-east-1"
